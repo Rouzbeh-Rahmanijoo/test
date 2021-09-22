efbf35e3d9449d5b76dedebf13fcc813a039f3d4
Rouzbeh-Rahmanijoo <42234623+Rouzbeh-Rahmanijoo@users.noreply.github.com>
2021-09-22 19:13:15 +0330

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
+from os import times
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import mysql.connector 
 from devidenumber import devidenumber
+import time
 
 mydb = mysql.connector.connect(
   host="localhost",
@@ -24,16 +26,18 @@ password = driver.find_element_by_id("UserP")
 password.send_keys("MIL@D82525")
 
 password.send_keys(Keys.ENTER)
+while(True):
+    time.sleep(30)
+    vaziatebikar = driver.find_element_by_class_name("tablesorter-header-inner")
 
-vaziatebikar = driver.find_element_by_class_name("tablesorter-header-inner")
+    vaziatebikar_value = vaziatebikar.get_attribute('innerHTML')
 
-vaziatebikar_value = vaziatebikar.get_attribute('innerHTML')
+    
+    mycursor = mydb.cursor()
 
+    sql = "INSERT INTO elements (name, value) VALUES (%s, %s)"
+    val = ("نفرات بی کار", devidenumber(vaziatebikar_value))
+    mycursor.execute(sql, val)
+    # vaziatebikar.send_keys(Keys.F5)
 
-mycursor = mydb.cursor()
-
-sql = "INSERT INTO elements (name, value) VALUES (%s, %s)"
-val = ("نفرات بی کار", devidenumber(vaziatebikar_value))
-mycursor.execute(sql, val)
-
-mydb.commit()
+    mydb.commit() 
